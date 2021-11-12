37b8c146edca9e61bc6c3f8e5d6786e6ca630898
Фёдор Алексеев <f.alexeeff2010@yandex.ru>
2021-11-13 00:48:01 +0300

    массив с тайлами заднего фона

--- csheroes/form/exploreForm.cs
+++ csheroes/form/exploreForm.cs
@@ -20,9 +20,13 @@ namespace csheroes.form
 
         Graphics surface;
 
+        Rectangle[,] background = null;
+
         public ExploreForm()
         {
             InitializeComponent();
+
+            InitBackground();
 
             surface = CreateGraphics();
         }
@@ -35,7 +39,7 @@ namespace csheroes.form
             DrawGrid();
         }
 
-        public void DrawGrid()
+        void DrawGrid()
         {
             for (int i = 0; i < Width / cellSize; i++)
                 surface.DrawLine(gridPen, cellSize * i, 0, cellSize * i, Height);
@@ -44,13 +48,20 @@ namespace csheroes.form
                 surface.DrawLine(gridPen, 0, cellSize * i, Width, cellSize * i);
         }
 
-        public void DrawBackground()
+        void InitBackground()
         {
+            background = new Rectangle[Width / cellSize, Height / cellSize];
 
             for (int i = 0; i < Width / cellSize; i++)
-                for (int j = 0; j < Height / cellSize; j++) // TODO: массив tile
-                    surface.DrawImage(texture, new Rectangle(cellSize * j, cellSize * i, cellSize, cellSize), new Rectangle(cellSize * rand.Next(0, 2), cellSize * rand.Next(0, 2), cellSize, cellSize), GraphicsUnit.Pixel);
+                for (int j = 0; j < Height / cellSize; j++)
+                    background[i, j] = new Rectangle(cellSize * rand.Next(0, 2), cellSize * rand.Next(0, 2), cellSize, cellSize);
+        }
 
+        void DrawBackground()
+        {
+            for (int i = 0; i < Width / cellSize; i++)
+                for (int j = 0; j < Height / cellSize; j++)
+                    surface.DrawImage(texture, new Rectangle(cellSize * j, cellSize * i, cellSize, cellSize), background[i,j], GraphicsUnit.Pixel);
         }
     }
 }
