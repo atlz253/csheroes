f011d38cd02831c49ac2b62ef2cb8c445d3d3756
John Doe <johndoe@example.com>
2021-11-12 15:33:38 +0300

    Добавьте файлы проекта.

---
+++ csheroes/form/exploreForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace csheroes.form
+{
+    partial class ExploreForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "C:\\Users\\RDP\\source\\repos\\csheroes\\csheroes\\res\\textures.png";
+            this.pictureBox1.Location = new System.Drawing.Point(397, 216);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ExploreForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(800, 800);
+            this.MinimumSize = new System.Drawing.Size(800, 800);
+            this.Name = "ExploreForm";
+            this.Text = "Heroes";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ csheroes/form/exploreForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csheroes.form
+{
+    public partial class ExploreForm : Form
+    {
+        const int cellSize = 32;
+        
+        static Random rand = new Random();
+        static Pen gridPen = new Pen(Color.Black, 1);
+        static Image texture = Image.FromFile("../../../res/textures.png");
+
+        Graphics surface;
+
+        public ExploreForm()
+        {
+            InitializeComponent();
+
+            surface = CreateGraphics();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            surface.Clear(Color.White);
+
+            DrawBackground();
+            DrawGrid();
+        }
+
+        public void DrawGrid()
+        {
+            for (int i = 0; i < Width / cellSize; i++)
+                surface.DrawLine(gridPen, cellSize * i, 0, cellSize * i, Height);
+
+            for (int i = 0; i < Height / cellSize; i++)
+                surface.DrawLine(gridPen, 0, cellSize * i, Width, cellSize * i);
+        }
+
+        public void DrawBackground()
+        {
+
+            for (int i = 0; i < Width / cellSize; i++)
+                for (int j = 0; j < Height / cellSize; j++)
+                    surface.DrawImage(texture, new Rectangle(cellSize * j, cellSize * i, cellSize, cellSize), new Rectangle(cellSize * rand.Next(0, 1), cellSize * rand.Next(0, 1), cellSize, cellSize), GraphicsUnit.Pixel);
+
+        }
+    }
+}
